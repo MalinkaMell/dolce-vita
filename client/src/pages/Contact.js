8493ed7c5af3d70bd847f53fda00ffb00f5638de
Maimell <malinka77@gmail.com>
2020-04-29 07:47:09 -0700

    renaming some components

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "./styles.css";
-import ContactContainer from "../components/contact/Contactcontainer";
+import ContactContainer from "../components/contact/Container";
 
 const Contact = () => {
   return(
