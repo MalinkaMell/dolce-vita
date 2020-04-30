08bda29d1163ce3f933336f968f7c6e560936d53
Maimell <malinka77@gmail.com>
2020-04-30 13:39:07 -0700

    adding google verification

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+
 app.listen(PORT, () => {
   console.log(`Server running now on port ${PORT}!`);
 });
